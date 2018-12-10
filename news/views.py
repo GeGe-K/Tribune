25c4e9502db6c2831fb6ea90c7f1fef8de3af325
GeGe-K <gloriagivondo@gmail.com>
2018-12-10 15:43:55 +0300

    modified the news of day.

--- news/views.py
+++ news/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 import datetime as dt
-from .models import Article
+from .models import Article, NewsLetterRecipients
+from .forms import NewsLetterForm
+from .email import send_welcome_email
 
 # Create your views here.
+
+
 def news_of_day(request):
-   date = dt.date.today()
-   news = Article.todays_news()
-   return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+    date = dt.date.today()
+    news = Article.todays_news()
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name, email = email)
+            recipient.save()
+            send_welcome_email(name,email)
+            HttpResponseRedirect('news_of_day')
+    else:
+        form = NewsLetterForm()
+
+    return render(request, 'all-news/today-news.html', {"date": date, "news": news, "letterForm": form})
+
 
 def convert_dates(dates):
 
